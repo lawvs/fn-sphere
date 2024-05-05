3d344f6ba62050ce858b8f70876224c0af0edced
lawvs <18554747+lawvs@users.noreply.github.com>
2024-05-05 22:40:02 +0800

    refactor: use @fn-sphere/filter in playground

--- packages/playground/src/app.tsx
+++ packages/playground/src/app.tsx
@@ -1,31 +1,51 @@
+import { countNumberOfRules, type LooseFilterGroup } from "@fn-sphere/core";
+import { createFilter } from "@fn-sphere/filter";
 import { useState } from "react";
+import { FilterIcon } from "tdesign-icons-react";
+import { Button } from "tdesign-react";
 import "./app.css";
-import { Filter } from "./filter";
 import { useZodUI } from "./hooks/misc";
-import { dataFilters, genSampleData, presetSchema } from "./presets";
+import {
+  dataFilters,
+  genSampleData,
+  presetSchema,
+  type PresetData,
+} from "./presets";
 import { DataTable } from "./table";
+
+const { getRule, openFilter } = createFilter({
+  schema: presetSchema,
+  filterList: dataFilters,
+  deepLimit: Infinity,
+});
+
+const initialRule = await getRule();
 
 export const App = () => {
   useZodUI();
 
   const [tableData] = useState(genSampleData);
-  const [filter, setFilter] = useState({
-    filterData: (d: typeof tableData) => d,
-  });
-  const restData = filter.filterData(tableData);
+  const [flattenFilter, setFlattenFilter] = useState<{
+    rule: LooseFilterGroup;
+    predicate: (data: PresetData) => boolean;
+  }>(initialRule);
+  const filteredData = tableData.filter(flattenFilter.predicate);
 
   return (
     <div className="wrapper">
       <h1>Filter Demo</h1>
       {/* <Column /> */}
-      <Filter
-        schema={presetSchema}
-        filterList={dataFilters}
-        onChange={(data) => {
-          setFilter(data);
+      <Button
+        icon={<FilterIcon />}
+        onClick={async () => {
+          // setOpen(!open);
+          const value = await openFilter();
+          setFlattenFilter(value);
         }}
-      />
-      <DataTable data={restData} />
+      >
+        Flatten Filter({countNumberOfRules(flattenFilter.rule)})
+      </Button>
+      <DataTable data={filteredData} />
     </div>
   );
 };
