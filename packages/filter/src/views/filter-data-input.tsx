191f8702e33bf0f72392c75fc2e774e26bf86a38
lawvs <18554747+lawvs@users.noreply.github.com>
2024-08-13 02:14:09 +0800

    chore: remove unnecessary forwardRef usage in FilterDataInput

--- packages/filter/src/views/filter-data-input.tsx
+++ packages/filter/src/views/filter-data-input.tsx
@@ -1,5 +1,4 @@
 import { getParametersExceptFirst, type SingleFilter } from "@fn-sphere/core";
-import { forwardRef } from "react";
 import { useFilterRule } from "../hooks/use-filter-rule.js";
 import { useDataInputView, useView } from "../theme/hooks.js";
 
@@ -7,37 +6,38 @@ export type DataInputProps = {
   rule: SingleFilter;
 };
 
-export const FilterDataInput = forwardRef<HTMLInputElement, DataInputProps>(
-  ({ rule }, ref) => {
-    const { Input: InputView } = useView("components");
-    const { selectedField, selectedFilter, setRule } = useFilterRule(rule);
-    const requiredArguments = selectedFilter
-      ? getParametersExceptFirst(selectedFilter)
-      : undefined;
-    const DataInputView = useDataInputView(
-      requiredArguments,
-      selectedField?.fieldSchema,
-    );
+export const FilterDataInput = ({ rule }: DataInputProps) => {
+  const { Input: InputView } = useView("components");
+  const { selectedField, selectedFilter, setRule } = useFilterRule(rule);
+  const requiredArguments = selectedFilter
+    ? getParametersExceptFirst(selectedFilter)
+    : undefined;
+  const DataInputView = useDataInputView(
+    requiredArguments,
+    selectedField?.fieldSchema,
+  );
 
-    const setRuleArgs = (value: unknown[]) => {
-      setRule({
-        ...rule,
-        args: value,
-      });
-    };
+  const setRuleArgs = (value: unknown[]) => {
+    // Can not check the schema here because the input value is not always valid
+    // For example, z.string().email() will throw an error when user types before the user types the complete email
+    // const requiredArgsSchema = z.tuple(requiredArguments);
+    // requiredArgsSchema.parse(value);
+    setRule({
+      ...rule,
+      args: value,
+    });
+  };
 
-    if (!requiredArguments) {
-      return <InputView ref={ref} disabled />;
-    }
+  if (!requiredArguments) {
+    return <InputView disabled />;
+  }
 
-    return (
-      <DataInputView
-        ref={ref}
-        rule={rule}
-        requiredDataSchema={requiredArguments}
-        updateInput={setRuleArgs}
-      />
-    );
-  },
-);
+  return (
+    <DataInputView
+      rule={rule}
+      requiredDataSchema={requiredArguments}
+      updateInput={setRuleArgs}
+    />
+  );
+};
 FilterDataInput.displayName = "FilterDataInput";
