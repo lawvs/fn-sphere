72137c56c52efc3782d93d0fbf6cd18287fb056d
Whitewater <me@waterwater.moe>
2024-08-23 17:25:17 +0800

    feat: make doc site with starlight (#46)
    * feat: add starlight docs template
    
    * chore: update starlight config
    
    * feat: add intro section
    
    * feat: update home page and getting started
    
    * chore: update live code config
    
    * feat: add intro example
    
    * feat: add showcase
    
    * refactor: extract PackageManagerTabs
    
    * chore: clean code

---
+++ packages/docs/astro.config.ts
@@ -0,0 +1,42 @@
+import react from "@astrojs/react";
+import starlight from "@astrojs/starlight";
+// @ts-expect-error missing types
+import liveCode from "astro-live-code";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Filter Sphere",
+      social: {
+        github: "https://github.com/lawvs/fn-sphere",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          autogenerate: {
+            directory: "guides",
+          },
+        },
+        {
+          label: "Reference",
+          autogenerate: {
+            directory: "reference",
+          },
+        },
+      ],
+      customCss: [
+        // Relative path to your custom CSS file
+        "./src/styles/custom.css",
+      ],
+    }),
+    react(),
+    liveCode({
+      wrapper: "/src/components/code-wrapper.tsx",
+      defaultProps: {
+        "client:load": true,
+      },
+    }),
+  ],
+});
---
+++ packages/docs/src/components/intro-example/code.tsx
@@ -0,0 +1,39 @@
+import {
+  FilterBuilder,
+  FilterSphereProvider,
+  useFilterSphere,
+} from "@fn-sphere/filter";
+import { z } from "zod";
+import { Table } from "./table";
+import { data } from "./utils";
+
+// 1. define the schema for your data
+const schema = z.object({
+  id: z.number(),
+  name: z.string(),
+  createdAt: z.date(),
+  status: z.union([
+    z.literal("pending"),
+    z.literal("completed"),
+    z.literal("cancelled"),
+  ]),
+});
+
+export type Data = z.infer<typeof schema>;
+
+export function AdvancedFilter() {
+  // 2. use the `useFilterSphere` hook to get the context and predicate
+  const { context, predicate } = useFilterSphere({ schema });
+  return (
+    <>
+      <div className="not-content" style={{ marginTop: "1rem" }}>
+        {/* 3. display the FilterBuilder inside the FilterSphereProvider */}
+        <FilterSphereProvider context={context}>
+          <FilterBuilder />
+        </FilterSphereProvider>
+      </div>
+      {/* 4. use the predicate to filter the data */}
+      <Table data={data.filter(predicate)} />
+    </>
+  );
+}
