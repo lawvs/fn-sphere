54a4eb728d07f599eb7ff3abd0e6e3c6563a63c5
lawvs <18554747+lawvs@users.noreply.github.com>
2024-08-29 20:48:32 +0800

    feat: add hook tests

---
+++ packages/filter/src/__tests__/hooks.test.tsx
@@ -0,0 +1,86 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { renderHook } from "@testing-library/react";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { z } from "zod";
+import {
+  FilterSchemaProvider,
+  useFilterRule,
+  useFilterSphere,
+} from "../index.js";
+
+// see https://vitest.dev/api/vi.html#vi-mock
+vi.mock(import("@fn-sphere/core"), async (importOriginal) => {
+  let id = 1;
+  beforeEach(() => {
+    // reset id
+    id = 1;
+  });
+
+  const mod = await importOriginal();
+  return {
+    ...mod,
+    // replace some exports
+    createSingleFilter: vi.fn((ruleInput) => ({
+      ...mod.createSingleFilter(ruleInput),
+      id: String(id++),
+    })),
+    createFilterGroup: vi.fn((groupInput) => ({
+      ...mod.createFilterGroup(groupInput),
+      id: String(id++),
+    })),
+  };
+});
+
+describe("useFilterSphere", async () => {
+  it("should return the total rule count and valid rule count correctly", async () => {
+    // see https://testing-library.com/docs/react-testing-library/intro
+    const { result } = renderHook(() =>
+      useFilterSphere({
+        schema: z.object({ name: z.string() }),
+      }),
+    );
+    expect(result.current.totalRuleCount).toEqual(1);
+    expect(result.current.validRuleCount).toEqual(0);
+  });
+});
+
+describe("useFilterRule", async () => {
+  it("should init state match snapshot", async () => {
+    // @ts-expect-error just for test
+    const { result } = renderHook(() => useFilterRule({ id: "1" }), {
+      wrapper: ({ children }) => {
+        const { context } = useFilterSphere({
+          schema: z.object({ name: z.string() }),
+        });
+        return (
+          <FilterSchemaProvider context={context}>
+            {children}
+          </FilterSchemaProvider>
+        );
+      },
+    });
+
+    expect(result.current.ruleState).toMatchInlineSnapshot(`
+      {
+        "depth": 1,
+        "index": 0,
+        "isFirstRule": true,
+        "isInvert": undefined,
+        "isLastRule": true,
+        "isValid": false,
+        "parentGroup": {
+          "conditions": [
+            {
+              "args": [],
+              "id": "1",
+              "type": "Filter",
+            },
+          ],
+          "id": "2",
+          "op": "and",
+          "type": "FilterGroup",
+        },
+      }
+    `);
+  });
+});
