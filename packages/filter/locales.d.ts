e7b3c8653d9d46236659a999c1f699718a3c35ed
lawvs <18554747+lawvs@users.noreply.github.com>
2024-08-30 14:10:14 +0800

    fix: subpath export compatibility

---
+++ packages/filter/locales.d.ts
@@ -0,0 +1,4 @@
+/* eslint-disable */
+// package.json subpath exports compatibility for node10
+// @ts-ignore
+export * from "./dist/locales";
---
+++ packages/filter/locales.js
@@ -0,0 +1,4 @@
+/* eslint-disable */
+/// <reference types="./dist/locales/index.d.ts" />
+// package.json subpath exports compatibility for node10
+export * from "./dist/locales/index.js";
